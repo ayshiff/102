3c31197470e39a11880281058c841a0f3c9cf7dd
ayshiff <remi.d45@gmail.com>
2018-01-11 09:10:54 +0100

    Update test-curry.js

--- test/test-curry.js
+++ test/test-curry.js
@@ -4,15 +4,13 @@ var curry = require('../curry.js')
 
 describe('curry function test', () => {
 
-    it('curry (f(x, y)) should return f(x)(y)', () => {
-        var arg1 = Math.random()
-        var arg2 = Math.random()
-        var curryMultiplication = curry(function(x, y, z) { return x * y; }, arg1, 4)
-        // var triple = curryMultiplication(arg1)
-        var supposedResult = curryMultiplication(arg2)
-        var result = curryMultiplication(arg1, 4, arg2)
-        // var result = composed(arg1, arg2)
-        expect(result).to.equal(supposedResult)
+    it('curry (f)(x) should return f(x)', () => {
+        var arg = Math.random()
+        var f = function(x) { return x}
+        var expectedResult = f(arg)
+        var curr = curry(f)
+        var expected = curr(arg)
+        expect(expected).to.equal(expectedResult)
     })
 // Probleme here
     /*
